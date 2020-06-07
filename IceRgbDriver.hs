16b4b96f02849918abc309fa34a090f864051043
dopamane <dwc1295@gmail.com>
2020-06-07 09:18:07 -0400

    primitive working

--- IceRgbDriver.hs
+++ IceRgbDriver.hs
@@ -1,30 +1,70 @@
-{-# LANGUAGE CPP           #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
 module IceRgbDriver (Rgb, iceRgbDriver) where
 
 import Clash.Prelude
-
-#ifdef CABAL
 import Clash.Annotations.Primitive
-import System.FilePath
-import qualified Paths_blinker
-import System.IO.Unsafe
+import Data.String.Interpolate (i)
+import Data.String.Interpolate.Util (unindent)
 
-{-# ANN iceRgbDriverPrim hasBlackBox #-}
-{-# ANN module (Primitive [Verilog] (unsafePerformIO Paths_blinker.getDataDir </> "prim")) #-}
-#endif
+{-# ANN iceRgbDriverPrim (InlinePrimitive [Verilog] $ unindent [i|
+  [ { "BlackBox" :
+      { "name" : "IceRgbDriver.iceRgbDriverPrim"
+      , "kind" : "Declaration"
+      , "type" :
+  "iceRgbDriverPrim
+  :: String         -- current_mode ARG[0]
+  -> String         -- rgb0_current ARG[1]
+  -> String         -- rgb1_current ARG[2]
+  -> String         -- rgb2_current ARG[3]
+  -> Signal dom Bit -- pwm_r        ARG[4]
+  -> Signal dom Bit -- pwm_g        ARG[5]
+  -> Signal dom Bit -- pwm_b        ARG[6]
+  -> Signal dom (Bit, Bit, Bit)"
+      , "template" :
+  "//SB_RGBA_DRV begin
+  wire ~GENSYM[BLUE][2];
+  wire ~GENSYM[GREEN][1];
+  wire ~GENSYM[RED][0];
 
-type Rgb = ("red" ::: Bit, "green" ::: Bit, "blue" ::: Bit)
+  SB_RGBA_DRV #(
+     .CURRENT_MODE(~ARG[0]),
+     .RGB0_CURRENT(~ARG[1]),
+     .RGB1_CURRENT(~ARG[2]),
+     .RGB2_CURRENT(~ARG[3])
+  ) RGBA_DRIVER (
+     .CURREN(1'b1),
+     .RGBLEDEN(1'b1),
+     .RGB0PWM(~ARG[6]),
+     .RGB1PWM(~ARG[5]),
+     .RGB2PWM(~ARG[4]),
+     .RGB0(~SYM[2]),
+     .RGB1(~SYM[1]),
+     .RGB2(~SYM[0])
+  );
+ 
+  assign ~RESULT = {~SYM[0], ~SYM[1], ~SYM[2]};
+  //SB_RGBA_DRV end"
+      }
+    } 
+  ]
+  |]) #-}
 
 {-# NOINLINE iceRgbDriverPrim #-}
 iceRgbDriverPrim
-  :: Signal dom Bit
+  :: String
+  -> String
+  -> String
+  -> String
+  -> Signal dom Bit
   -> Signal dom Bit
   -> Signal dom Bit
   -> Signal dom (Bit, Bit, Bit)
-iceRgbDriverPrim r g b = bundle (r, g, b)
+iceRgbDriverPrim _ _ _ _ _ _ _ = pure (0, 0, 0)
+
+type Rgb = ("red" ::: Bit, "green" ::: Bit, "blue" ::: Bit)
 
 iceRgbDriver :: Signal dom Rgb -> Signal dom Rgb
 iceRgbDriver rgb = let (r, g, b) = unbundle rgb
-                    in iceRgbDriverPrim r g b
+                    in iceRgbDriverPrim "0b0" "0b111111" "0b111111" "0b111111" r g b
